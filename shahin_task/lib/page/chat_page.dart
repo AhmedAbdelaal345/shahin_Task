7accb7c4924cc6329e60ef421afc66e9b2d07e36
AhmedAbdelaal345 <aabdelaal145@gmail.com>
2025-10-21 13:31:03 +0300

    second

--- shahin_task/lib/page/chat_page.dart
+++ shahin_task/lib/page/chat_page.dart
@@ -5,6 +5,10 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text('Chat Page'),
+      ),
+    );
   }
 }
